ccdf2d9cf3fb8c5e7073ff98ad8994bafe893f21
Bishesh <bishesh.sunam@gmail.com>
2022-03-05 18:10:47 +1300

    added responsiveness on banner

--- components/Banner.js
+++ components/Banner.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 function Banner() {
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-4">
-      <div className="m-4 flex h-[500px] justify-between pt-10">
+      <div className="m-4 flex h-[500px] justify-between pt-10 flex-col md:flex-row">
         <div className="flex-[0.6] p-10 font-Roboto">
           <h1 className=" py-8 text-5xl font-bold">Hey, I'm Bishesh</h1>
           <div className="font-sans text-xl text-gray-200">
@@ -12,14 +12,14 @@ function Banner() {
               buids & solve problems using latest technologies.
             </p>
             <p>I'm looking for new opportunities to sky-rocket my career!</p>
-            <div className="flex space-x-8 py-4 mt-2">
-              <button className="btn">Expore my work</button>
-              <button className="btn ">Get in touch</button>
+            <div className="md:flex md:space-x-8 py-4">
+              <button className="btn m-2">Expore my work</button>
+              <button className="btn m-2">Get in touch</button>
             </div>
           </div>
         </div>
-        <div className="flex-[0.4]">
-          <div className="relative m-4 h-96 w-96 overflow-hidden rounded-full border-8">
+        <div className="md:flex-[0.4] flex justify-center items-center">
+          <div className="relative m-4 h-52 w-52 md:h-96 md:w-96 overflow-hidden rounded-full border-8">
             <Image src={bishesh} alt="" layout="responsive" className="" />
           </div>
         </div>
