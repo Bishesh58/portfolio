8a2a0e600108430303b352712c8a4dbb76b0fd13
Bishesh <bishesh.sunam@gmail.com>
2022-03-16 22:17:23 +1300

    added project layout

---
+++ components/Card.js
@@ -0,0 +1,10 @@
+
+function Card() {
+  return (
+    <div className="border w-96 h-96 m-2 rounded-2xl border-gray-300">
+        
+    </div>
+  )
+}
+
+export default Card
--- components/Projects.js
+++ components/Projects.js
@@ -1,10 +1,20 @@
-
+import Card from './Card'
 
 function Projects() {
   return (
     <div className="min-h-screen text-white p-4">
-      <div className="">
-        <h1 className="py-4 text-3xl font-bold">Projects</h1>
+      <div className="flex flex-col">
+        <h1 className="py-4 px-6 text-3xl font-bold">Projects</h1>
+        <div className="flex flex-wrap p-2">
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
       </div>
     </div>
   )
