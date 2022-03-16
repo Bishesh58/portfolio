a549df3d938ff37fdb94111d7a18b4a703ace327
Bishesh <bishesh.sunam@gmail.com>
2022-03-16 23:00:11 +1300

    added styled to project section

--- components/Card.js
+++ components/Card.js
@@ -1,8 +1,13 @@
-
 function Card() {
   return (
-    <div className="border w-96 h-96 m-2 rounded-2xl border-gray-300">
-        
+    <div className="flex flex-col m-2 h-96 w-96 rounded-2xl border border-gray-300 p-4">
+      <div className="flex-[0.9] rounded-2xl  bg-slate-600 ">
+        <img src="" alt="" />
+      </div>
+      <div className="flex-[0.1] flex justify-evenly my-6">
+        <button className="link-btn px-8">Live demo</button>
+        <button className="link-btn px-8">GitHub</button>
+      </div>
     </div>
   )
 }
--- components/Projects.js
+++ components/Projects.js
@@ -5,7 +5,7 @@ function Projects() {
     <div id="projects" className="min-h-screen text-white p-4">
       <div className="flex flex-col">
         <h1 className="py-4 px-6 text-3xl font-bold">Projects</h1>
-        <div className="flex flex-wrap p-2">
+        <div className="flex flex-wrap ">
           <Card/>
           <Card/>
           <Card/>
