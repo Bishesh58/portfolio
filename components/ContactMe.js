23a039944f1b7cfa9ca26a4c282eda642d2a4b13
Bishesh <bishesh.sunam@gmail.com>
2022-07-04 21:27:17 +1200

    added modal for contact form

--- components/ContactMe.js
+++ components/ContactMe.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
 import Wave from 'react-wavify'
 import Button from '@mui/material/Button'
+import LinearProgress from '@mui/material/LinearProgress'
+import Modal from '@mui/material/Modal'
 import axios from 'axios'
 
 export default function ContactMe() {
+  const [loading, setLoading] = useState(false)
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
+
   const sendMessage = async (e) => {
     e.preventDefault()
 
@@ -17,11 +24,25 @@ export default function ContactMe() {
     }
     console.log(data)
     try {
-      await axios.post(
-        'https://us-central1-portfolio-e2265.cloudfunctions.net/contactEmail',
-        data
-      )
+      setLoading(true)
+      await axios
+        .post(
+          'https://us-central1-portfolio-e2265.cloudfunctions.net/contactEmail',
+          data
+        )
+        .then((result) => {
+          setLoading(false)
+          handleOpen()
+          setName('')
+          setEmail('')
+          setMessage('')
+        })
+        .catch((error) => {
+          setLoading(false)
+          console.log(error)
+        })
     } catch (error) {
+      setLoading(false)
       console.log(error.message)
     }
   }
@@ -59,6 +80,7 @@ export default function ContactMe() {
             className="input"
             id="name"
             type="text"
+            value={name}
             onChange={(e) => setName(e.target.value)}
           />
           <label className="label" htmlFor="email">
@@ -68,6 +90,7 @@ export default function ContactMe() {
             className="input"
             id="email"
             type="email"
+            value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
           <label className="label" htmlFor="msg">
@@ -78,16 +101,38 @@ export default function ContactMe() {
             className="input"
             id="msg"
             type="text"
+            value={message}
             onChange={(e) => setMessage(e.target.value)}
           />
 
-          <Button
-            className="btnHover my-4 w-52 rounded-lg border bg-slate-500 p-3 text-white"
-            onClick={sendMessage}
-          >
-            Send message
-          </Button>
+          {loading ? (
+            <div className="my-4 w-full rounded-lg border bg-slate-500 p-3 text-white">
+              {' '}
+              <LinearProgress />{' '}
+            </div>
+          ) : (
+            <Button
+              className="btnHover my-4 w-52 rounded-lg border bg-slate-500 p-3 text-white"
+              onClick={sendMessage}
+              disabled={loading}
+            >
+              Send message
+            </Button>
+          )}
         </form>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          className="mt-20 flex h-16 w-full items-center justify-center bg-green-400 text-white"
+          style={{ alignItems: 'center', justifyContent: 'center' }}
+          disableAutoFocus={true}
+        >
+          <div className="border-none">
+            <p className="!border-none p-2 outline-none">
+              Your email has been send!
+            </p>
+          </div>
+        </Modal>
       </div>
     </div>
   )
